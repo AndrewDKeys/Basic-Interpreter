0270e144b38b358e1b57a486bb5185e428c36cd9
AndrewDKeys <adkeys04@gmail.com>
2024-04-16 12:12:30 -0400

    Finished Read Interpreter

--- Interpreter/Interpreter.java
+++ Interpreter/Interpreter.java
@@ -4,7 +4,6 @@ import Parser.Node.*;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Random;
 
 public class Interpreter {
@@ -30,6 +29,8 @@ public class Interpreter {
         this.statementList = statementList;
         gosubMap = gosubWalk();
         dataQueue = dataWalk();
+        intMap = new HashMap<String, Integer>();
+        floatMap = new HashMap<String, Float>();
     }
 
     //Walks the tree until a data statement is found, can return empty queue if none are used
@@ -55,26 +56,84 @@ public class Interpreter {
         return gosubMap;
     }
 
-    private int evaluateInt() {
+    private int evaluateInt(Node node) {
+        if(node instanceof IntegerNode) {
+           return ((IntegerNode) node).getValue();
+        } else if(node instanceof MathOpNode) {
 
-        return 0;
+            return 0;
+        } else if(node instanceof VariableNode) {
+
+            return 0;
+        } else if (node instanceof FunctionNode) {
+
+            return 0;
+        } else {
+            throw new RuntimeException("Invalid integer variable assignment");
+        }
     }
 
-    private float evaluateFloat() {
+    private float evaluateFloat(Node node) {
+        if(node instanceof FloatNode) {
+            return ((FloatNode) node).getValue();
+        } else if(node instanceof MathOpNode) {
 
-        return 0;
+            return 0;
+        } else if(node instanceof VariableNode) {
+
+            return 0;
+        } else if (node instanceof FunctionNode) {
+
+            return 0;
+        } else {
+            throw new RuntimeException("Invalid float variable assignment");
+        }
+    }
+
+    //returns the type of a variable i.e. String, int, float
+    private String evaluateVariableType(VariableNode node) {
+        char type = node.toString().charAt(node.toString().length() - 1); //the last character of a variable signifies its type
+        switch(type) {
+            case '%' -> { return "float"; }
+            case '$' -> { return "string"; }
+            default  -> { return "int"; }
+        }
+    }
+
+    private void evaluateRead(ReadNode node) {
+        var readList = node.getValue();
+        if(readList.size() != dataQueue.size())
+            throw new RuntimeException("READ list size does not match DATA list size"); //READ needs to have same amount of variables as DATA
+
+        for(int i = 0; i < dataQueue.size(); i++) {
+            Node data = dataQueue.remove(); //removes the first inputted element in queue style
+            if(data instanceof IntegerNode) {
+                if(evaluateVariableType(readList.get(i)).equals("int")) { //checks to see if read variable is the correct type
+
+                    intMap.put(readList.get(i).toString(), evaluateInt(data));
+                } else {
+                    throw new RuntimeException("Mismatched types in READ list");
+                }
+            } else if(data instanceof FloatNode) {
+                if(evaluateVariableType(readList.get(i)).equals("float")) { //checks to see if read variable is the correct type
+                    floatMap.put(readList.get(i).toString(), evaluateFloat(data));
+                } else {
+                    throw new RuntimeException("Mismatched types in READ list");
+                }
+            } else {
+                if(evaluateVariableType(readList.get(i)).equals("string")) { //checks to see if read variable is the correct type
+                    stringMap.put(readList.get(i).toString(), data.toString());
+                } else {
+                    throw new RuntimeException("Mismatched types in READ list");
+                }
+            }
+        }
     }
 
     public void interpret() {
         for(Node node : statementList.getList()) {
             if(node instanceof ReadNode) {
-                var readList = ((ReadNode) node).getValue();
-                if(readList.size() != dataQueue.size()) {
-                    throw new RuntimeException(); //READ needs to have same amount of variables as DATA
-                }
-                for (int i = 0; i < dataQueue.size(); i++) {
-
-                }
+                evaluateRead((ReadNode) node);
             }
         }
     }
